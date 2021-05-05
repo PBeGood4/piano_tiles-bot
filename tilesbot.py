47ba66a8838d4806fa255cc388f9566aea8a3be5
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-05-05 20:34:45 +0100

    Update tilesbot.py

--- tilesbot.py
+++ tilesbot.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
 
 import random
-import cv2 as cv
-import pyautogui
 import mss
 import mss.tools
 import threading
 import time
 from pynput import mouse,keyboard
 from pynput.mouse import Controller, Button
-import sys
-
-sys.platform ="_"
-
 
 mouse = Controller()
   
+offset=70
 
-    
+def mainFunc(timeDelay):  
+    sct_img = sct.grab(monitor) 
+    if sct_img.pixel(int(monitor["width"]/4),int(monitor["height"]/10))[0]<10:
+            mouse.position = (int(monitor["width"]/8)  ,int(monitor["height"]/10)+monitor["top"]+offset)
+            time.sleep(1)
+            mouse.click(Button.left)    
+    elif sct_img.pixel(int(monitor["width"]/4*3),int(monitor["height"]/10))[0]<10:
+            mouse.position = (int(monitor["width"]/8*3),int(monitor["height"]/10)+monitor["top"]+offset)
+            time.sleep(1)
+            mouse.click(Button.left)
+    elif sct_img.pixel(int(monitor["width"]/4*5),int(monitor["height"]/10))[0]<10:
+            mouse.position = (int(monitor["width"]/8*5),int(monitor["height"]/10)+monitor["top"]+offset)
+            time.sleep(1)
+            mouse.click(Button.left)
+    elif sct_img.pixel(int(monitor["width"]/4*7),int(monitor["height"]/10))[0]<10:
+            mouse.position = (int(monitor["width"]/8*7),int(monitor["height"]/10)+monitor["top"]+offset)
+            time.sleep(1)
+            mouse.click(Button.left)     
 
-def mainFunc(timeDelay):        
     while True:
         if a:
             break
@@ -41,17 +52,12 @@ def mainFunc(timeDelay):
 
 
 a=False
-spaceTimes=0
 pos1 = (0,0)
 pos2 = (0,0)
 def on_press(key):
     if key == keyboard.Key.space:
-        global spaceTimes
         global pos1
         global pos2
-        global pos3
-        global pos4
-        spaceTimes+=1
         if pos1 ==(0,0):
             pos1 = mouse.position
             print("Position 1 = "+str(mouse.position))
@@ -75,6 +81,8 @@ def listen():
 
 threading.Thread(target=listen).start()
 
+print("\nSelect the boundaries\n\n1. Top Left\n2. Bottom Right\n")
+
 while pos2==(0,0):
     pass
 
@@ -84,8 +92,24 @@ with mss.mss() as sct:
     output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
 
 time.sleep(1)
-print("Starting...")
+print("\nPress ESC to exit\n  ")
+time.sleep(1)
+print("Starting in 5")
+time.sleep(1)
+print("Starting in 4")
+time.sleep(1)
+print("Starting in 3")
+time.sleep(1)
+print("Starting in 2")
+time.sleep(1)
+print("Starting in 1")
+time.sleep(1)
+print(".......")
+
 mainFunc(0)
+
+
+
 
 
 
