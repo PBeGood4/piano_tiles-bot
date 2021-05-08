9a3526ee4534a59672bfa4dee52f6557ac9de034
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-05-08 16:00:23 +0100

    Update tilesbot.py

--- tilesbot.py
+++ tilesbot.py
@@ -9,25 +9,26 @@ from pynput import mouse,keyboard
 from pynput.mouse import Controller, Button
 
 mouse = Controller()
-  
-offset=70
+
 
 def mainFunc(timeDelay):  
-    sct_img = sct.grab(monitor) 
-    if sct_img.pixel(int(monitor["width"]/4),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8)  ,int(monitor["height"]/10)+monitor["top"]+offset)
+    global monitor
+    offset=monitor["height"]-10
+    sct_img = sct.grab(monitor)   
+    if sct_img.pixel(int(monitor["width"]/4),int(monitor["height"]/10))[0]  <10:
+            mouse.position = (int(monitor["width"]/8)  +int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             time.sleep(1)
             mouse.click(Button.left)    
     elif sct_img.pixel(int(monitor["width"]/4*3),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8*3),int(monitor["height"]/10)+monitor["top"]+offset)
+            mouse.position = (int(monitor["width"]/8*3) +int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             time.sleep(1)
             mouse.click(Button.left)
     elif sct_img.pixel(int(monitor["width"]/4*5),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8*5),int(monitor["height"]/10)+monitor["top"]+offset)
+            mouse.position = (int(monitor["width"]/8*5) +int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             time.sleep(1)
             mouse.click(Button.left)
     elif sct_img.pixel(int(monitor["width"]/4*7),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8*7),int(monitor["height"]/10)+monitor["top"]+offset)
+            mouse.position = (int(monitor["width"]/8*7) +int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             time.sleep(1)
             mouse.click(Button.left)     
 
@@ -37,17 +38,18 @@ def mainFunc(timeDelay):
         sct_img = sct.grab(monitor)
 
         if sct_img.pixel(int(monitor["width"]/4),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8)  ,int(monitor["height"]/10)+monitor["top"]+70)
+            mouse.position = (int(monitor["width"]/8)  +int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             mouse.click(Button.left)    
         elif sct_img.pixel(int(monitor["width"]/4*3),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8*3),int(monitor["height"]/10)+monitor["top"]+70)
+            mouse.position = (int(monitor["width"]/8*3)+int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             mouse.click(Button.left)
         elif sct_img.pixel(int(monitor["width"]/4*5),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8*5),int(monitor["height"]/10)+monitor["top"]+70)
+            mouse.position = (int(monitor["width"]/8*5)+int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset)
             mouse.click(Button.left)
         elif sct_img.pixel(int(monitor["width"]/4*7),int(monitor["height"]/10))[0]<10:
-            mouse.position = (int(monitor["width"]/8*7),int(monitor["height"]/10)+monitor["top"]+70  )
+            mouse.position = (int(monitor["width"]/8*7)+int(monitor["left"]),int(monitor["height"]/10)+monitor["top"]+offset  )
             mouse.click(Button.left)
+  
 
 
 
@@ -55,6 +57,7 @@ a=False
 pos1 = (0,0)
 pos2 = (0,0)
 def on_press(key):
+    global offset
     if key == keyboard.Key.space:
         global pos1
         global pos2
@@ -64,12 +67,23 @@ def on_press(key):
         elif pos2 ==(0,0):
             pos2 = mouse.position 
             print("Position 2 = "+str(mouse.position))
+            
+
+    if key == keyboard.Key.shift_r   and offset+5<monitor["height"]:
+        offset+=5
+        print(offset)
+
+    if key == keyboard.Key.shift and offset-5>  0:
+        offset-=5
+        print(offset)
 
 def on_release(key):
     global a
     if key == keyboard.Key.esc:
         a=True
         return False
+
+    
 
 
 listener = keyboard.Listener(
